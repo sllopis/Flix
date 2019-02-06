13aa8a26a607f932e476d0fda1a1e902882707e8
Sergio <sllopis@csumb.edu>
2019-02-06 14:25:13 -0800

    Flix Part 2 Done

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Sergio Llopis on 2/4/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
